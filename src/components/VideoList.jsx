c8d3db7c56721769c625d80725237d8efd783c17
Fleming <flemingsyiemlieh8899@gmail.com>
2025-05-11 23:16:10 +0530

    responsive layout with custom breakpoints, setting nav, side and category as fixed layout

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -6,7 +6,7 @@ const VideoList = () => {
     const toggle = useSelector((state) => state.toggle.sidebar);
 
     return (
-        // will move to the left as per our fixed sidebar 
+        // will move to the left as per our fixed sidebar but only on bigger screen
         <div className={`relative top-38 ${!toggle ? "md:ml-18" : "md:ml-64"}  grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8`}>
             {
                 videos && videos.map((item) => (
